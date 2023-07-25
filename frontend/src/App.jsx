6d1e5b2a1abcb1d6866d2a8020151dcc53ba93a1
Nithin Prathapan <nithinprathapan369@gmail.com>
2023-07-25 20:15:34 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Note-Down</h1>
+        <div className="input">
+          <input type="text" />
+          <button>Add</button>
+        </div>
+        <div className="list">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
