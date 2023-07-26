b5e2babe64f86784006151dc1f69a1076c25032b
Nithin Prathapan <nithinprathapan369@gmail.com>
2023-07-27 01:59:40 +0530

    add functionality completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Todo from "./components/Todo";
 import "./App.css";
-import { getAllTodos } from "./utils/HandleApi";
+import { saveTodo, getAllTodos, updateTodo } from "./utils/HandleApi";
 const App = () => {
   const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [todoId, setTodoId] = useState(false);
   useEffect(() => {
     getAllTodos(setTodo);
   }, []);
@@ -12,8 +15,22 @@ const App = () => {
       <div className="container">
         <h1>Note-Down</h1>
         <div className="input">
-          <input type="text" placeholder="NOTE DOWN HERE...🖊️" />
-          <button onClick={addTodo}>Add</button>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="NOTE DOWN HERE...🖊️"
+          />
+          <button
+            onClick={
+              isUpdating
+                ? () =>
+                    updateTodo(todoId, text, setTodo, setText, setIsUpdating)
+                : () => saveTodo(text, setText, setTodo)
+            }
+          >
+            {isUpdating ? "Update" : "Add"}
+          </button>
         </div>
         <div className="list">
           {todo.map((item) => (
--- frontend/src/utils/HandleApi.jsx
+++ frontend/src/utils/HandleApi.jsx
@@ -7,4 +7,27 @@ const getAllTodos = async (setTodo) => {
     setTodo(data.data);
   });
 };
-export { getAllTodos };
+const saveTodo = async (text, setText, setTodo) => {
+  try {
+    await axios.post(`http://localhost:5000/save`, { text }).then((data) => {
+      console.log(data);
+      setText("");
+      getAllTodos(setTodo);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateTodo = async (todoId, text, setTodo, setText, setIsUpdating) => {
+  try {
+    await axios.patch(`${baseUrl}/update/${todoId}`, text).then((data) => {
+      console.log(data);
+      setIsUpdating(false);
+      setText("");
+      getAllTodos(setTodo);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export { getAllTodos, saveTodo, updateTodo };
